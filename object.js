d180b8c8a493624cdca5fe53f104ce538c9a3c89
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-15 00:39:04 +0300

    Add function to loop over array

--- object.js
+++ object.js
@@ -1,24 +1,25 @@
-const books = [
-{
-  title: 'Gifted hands',
-  author: 'Ben Carson',
-  button: 'Remove',
-  line:    'hr'
-},
+const bookList = [
+  {
+    title: 'Gifted hands',
+    author: 'Ben Carson',
+    button: 'Remove',
+    line: 'hr',
+  },
 
-{
-  title: 'Gifted hands',
-  author: 'Ben Carson',
-  button: 'Remove',
-  line:    'hr'
-},
+  {
+    title: 'Gifted hands',
+    author: 'Ben Carson',
+    button: 'Remove',
+    line: 'hr',
+  },
 
-{
-  title: 'Gifted hands',
-  author: 'Ben Carson',
-  button: 'Remove',
-  line:    'hr'
-},
+  {
+    title: 'Gifted hands',
+    author: 'Ben Carson',
+    button: 'Remove',
+    line: 'hr',
+  },
 
-]
+];
 
+const collection = document.getElementById('books') 
