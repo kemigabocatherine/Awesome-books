4c959fd1374dcfbbb1cc6ba456ae4be1b0c74a16
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-15 00:43:18 +0300

    Add function to loop over array

--- object.js
+++ object.js
@@ -22,4 +22,16 @@ const bookList = [
 
 ];
 
-const collection = document.getElementById('books') 
+const collection = document.getElementById('books');
+
+bookList.forEach(book, index), => {
+  const element = document.createElement('div');
+  element.classList.add('item');
+
+  element.innerHTML = `
+    <h4>${bookList[index].title}</h4>
+    <p>${bookList[index].author}</p>
+    <button type="button">${bookList[index].button}</button>
+    <${bookList[index].line}>
+  `
+}
