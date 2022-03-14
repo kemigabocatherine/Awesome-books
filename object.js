cb7d0f17d80d2a95d4439b90054b4e667aae9e3a
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-15 02:52:49 +0300

    Add functions for adding and removing books

--- object.js
+++ object.js
@@ -1,3 +1,5 @@
+const { createElement } = require("parse5/lib/tree-adapters/default");
+
 const bookList = [
   {
     title: 'Gifted hands',
@@ -31,7 +33,7 @@ bookList.forEach((book, index) => {
   element.innerHTML = `
     <h4>${bookList[index].title}</h4>
     <p>${bookList[index].author}</p>
-    <button type="button">${bookList[index].button}</button>
+    <button type="button" class="delete">${bookList[index].button}</button>
     <${bookList[index].line}>
   `;
   collection.appendChild(element);
@@ -40,8 +42,29 @@ bookList.forEach((book, index) => {
 
 const addBook = document.querySelector('.adding');
 addBook.textContent = 'Add';
-addBook.addEventListener('click', => {
+addBook.addEventListener('click',() => {
+  const newDiv = document.createElement('div');
+  const newTitle = document.getElementById('new-title').value;
+  const newAuthor = document.getElementById('new-author').value;
 
+  const heading = document.createElement('h4');
+  heading.appendChild(document.createTextNode(newTitle));
+
+  const paragraph = document.createElement('p');
+  paragraph.appendChild(document.createTextNode(newAuthor));
+
+  const remove = document.createElement('button');
+  remove.classList.add('delete');
+  remove.setAttribute('type','button');
+  remove.appendChild(document.createTextNode('Remove'));
+
+  const bottomLine = document.createElement('hr');
+
+  newDiv.appendChild(heading);
+  newDiv.appendChild(paragraph);
+  newDiv.appendChild(remove);
+  newDiv.appendChild(bottomLine);
+
+  collection.appendChild(newDiv)
 });
 
-for (let i = 0; i < )
