1a73d6bb478dd9393fa2f66e9f6c6d119b977a10
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-03-15 00:06:14 +0300

    Add an array of objects containing the books

---
+++ object.js
@@ -0,0 +1,18 @@
+const books = [
+{
+  title: 'Gifted hands',
+  author: 'Ben Carson',
+  b
+},
+
+{
+  title: 'Gifted hands',
+  author: 'Ben Carson',
+},
+
+{
+  title: 'Gifted hands',
+  author: 'Ben Carson',
+},
+
+]
