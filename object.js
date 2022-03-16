2da267e4f8a83ad72cee0ecfa5ab502777d82b2e
Have Samuel <samiehave9@gmail.com>
2022-03-16 19:16:38 +0300

    Changing the remove function

--- object.js
+++ object.js
@@ -1,27 +1,61 @@
 let books = [];
 
-if (localStorage.getItem("books")) {
-  books = JSON.parse(localStorage.getItem("books"));
+if (localStorage.getItem('books')) {
+  books = JSON.parse(localStorage.getItem('books'));
 }
 
-const addBookBtn = document.getElementById("add-book");
+const addBookBtn = document.getElementById('add-book');
+
+const renderBooks = (books) => {
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+  books.forEach((book) => {
+    addBook(book.title, book.author);
+  });
+};
+
+const deleteBook = (bookTitle) => {
+  const result = books.filter((book) => book.title !== bookTitle);
+  books = result;
+  localStorage.setItem('books', JSON.stringify(books));
+  renderBooks(books);
+};
 
 const addBook = (title, author) => {
-  const bookList = document.getElementById("book-list");
-  const book = document.createElement("div");
-  const bookTitle = document.createElement("p");
+  const bookList = document.getElementById('book-list');
+  const book = document.createElement('div');
+  const bookTitle = document.createElement('p');
   bookTitle.textContent = `Title: ${title}`;
-  const bookAuthor = document.createElement("p");
+  const bookAuthor = document.createElement('p');
   bookAuthor.textContent = `Author: ${author}`;
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Remove";
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
 
-  removeBtn.addEventListener("click", () => {
+  removeBtn.addEventListener('click', () => {
     deleteBook(title);
   });
 
-  const bookHr = document.createElement("hr");
+  const bookHr = document.createElement('hr');
 
   book.append(bookTitle, bookAuthor, removeBtn, bookHr);
   bookList.append(book);
 };
+
+const addBookBtnHandler = (e) => {
+  e.preventDefault();
+  const bookTitleInput = document.getElementById('title');
+  const bookAuthorInput = document.getElementById('author');
+  const bookObject = {
+    title: bookTitleInput.value,
+    author: bookAuthorInput.value,
+  };
+  books.push(bookObject);
+  localStorage.setItem('books', JSON.stringify(books));
+  renderBooks(books);
+  bookTitleInput.value = '';
+  bookAuthorInput.value = '';
+};
+
+addBookBtn.addEventListener('click', addBookBtnHandler);
+
+renderBooks(books);
